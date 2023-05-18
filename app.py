e7ba0c8b4efcddf36e09792285c492d50705fbbd
thing-zoo <cathy4025@gmail.com>
2023-05-18 18:04:55 +0900

    add guestbook post api and connect mongodb

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+from pymongo import MongoClient
+client = MongoClient('url')
+db = client.dbsparta
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -8,6 +12,20 @@ def home():
 @app.route('/thingzoo')
 def thingzoo():
     return render_template('thingzoo.html')
+
+@app.route('/thingzoo/guestbook', methods=["POST"])
+def thing_guestbook_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive,
+    }
+
+    db.guestbook.thingzoo.insert_one(doc)
+
+    return jsonify({'msg': '등록 완료!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
